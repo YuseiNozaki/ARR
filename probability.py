88f8ba7c9bda78ac45ea7712fb22550e6760326b
YuseiNozaki <yusei.9933@gmail.com>
2022-04-08 00:36:42 +0900

    add prob_value

--- probability.py
+++ probability.py
@@ -1,11 +1,13 @@
 from scipy.stats import norm
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
-from tqdm import tqdm
 
 from data import data, date
 
 
+start = 0
+end = 1440
 step = 0.01
 
 x = np.arange(0, 4000, step)
@@ -16,9 +18,11 @@ for d in tqdm(data):
     fx = np.array(norm.pdf(x, d, 100))
     y += fx
 
-
 y *= 100/(sum(y)*step)
 
+prob = sum(y[int(start/step) : int(end/step)]) * step
+
+print(f'{start}min ~ {end}min : {int(prob)}%')
 
 plt.plot(x, y)
 plt.title(f'Probability Distribution of Reply Time - {date}')
